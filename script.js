76a9282b8972d4424198dd3714ac1ded823cc4fa
Arthur <arthur.genaro.gg02@gmail.com>
2020-05-16 11:45:46 -0400

    iniciando

---
+++ script.js
@@ -0,0 +1,18 @@
+let convidados = []
+function adicionar(){
+    let nome = document.getElementById("nome").value
+    let idade = document.getElementById("idade").value
+    let email = document.getElementById("email").value
+    convidados.push({nome,idade,email})
+    atualizarLista()
+}
+function atualizarLista(){
+    let lista = document.getElementById("lista")
+    lista.innerHTML = ""
+    for(i=0;i<convidados.length;1++){
+        lista.innerHTML +=` 
+        <div>
+    <span>${convidados(i).nome} </span>
+        </div>`
+    }
+}
